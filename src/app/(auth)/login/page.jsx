c7ca21d99fe440569690680902e837c812fa7cba
Samincode01 <samin990bd@gmail.com>
2026-05-24 23:29:25 +0600

    Started the project

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            login
+
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            News details page
+        </div>
+    );
+};
+
+export default page;
