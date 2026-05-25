9659347ccbe1ddae4dc0339be47ebb4032d78364
Samincode01 <samin990bd@gmail.com>
2026-05-25 13:04:13 +0600

    Created header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import logo from "@/assets/logo.png"
+import Image from 'next/image';
 const Header = () => {
     return (
         <div>
-            Header
+            <Image src={logo} width={300} height={200} alt='logo'></Image>
+            <p>Journalism Without Fear or Favour</p>
+            <p>Date</p>
         </div>
     );
 };
