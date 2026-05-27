d6af7e4a9a09d03173156b395add89318e31461c
Samincode01 <samin990bd@gmail.com>
2026-05-27 12:39:13 +0600

    Updated news by category section

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -15,12 +15,12 @@ export default async function Home() {
         <LeftSideBar categories={categories} activeId={"01"} />
       </div>
 
-      <div className="col-span-6 bg-green-100 font-bold">
-        All News
+      <div className="col-span-6 bg-green-100">
+        <h1>News By Category</h1>
 
         <div className="space-y-4">
           {news?.map((n) => (
-            <NewsCard key={n._id} className="p-8 rounded-md border" news={n}>
+            <NewsCard key={n._id}  news={n}>
               {n.title}
             </NewsCard>
           ))}
